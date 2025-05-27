3a96197387621ecacc121f5e6876997e5b67f302
Donatus Okwe <braindona@gmail.com>
2025-05-27 04:23:09 -0700

    Model and interface setup for Product

---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,16 @@
+import { Document, Types } from "mongoose";
+
+export interface IProduct extends Document {
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  barcode: string;
+  category: Types.ObjectId;
+  discountedPrice?: number;
+  discountPercentage?: number;
+  minQuantity?: number;
+  isActive?: boolean;
+  isFeatured?: boolean;
+  images?: string[];
+}
