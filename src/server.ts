0be11b6744867dd4715b40d5b05f4db57052695b
Donatus Okwe <braindona@gmail.com>
2025-05-27 04:59:25 -0700

    Global error handler configuration

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,25 @@ import helmet from "helmet";
 
 const app = express();
 import connectDB from "./utils/db-connection.util";
+import AppError from "./utils/AppError.util";
+import errorHandler from "./middleware/errorHandler.middleware";
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
 const port = process.env.PORT || 3000;
+
+// Catch 404 and forward to error handler
+app.use((_: Request, _res: Response, next: NextFunction) => {
+  next(new AppError("This route does not exist", 404));
+});
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  errorHandler(err, req, res, next);
+});
 
 app.listen(port, () => {
   connectDB()
